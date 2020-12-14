e80de8a5831c26dba20305bc200280cd5d688203
jkfan2 <jkfan2@iflytek.com>
2020-12-14 22:21:13 +0800

    手机端路由切换之后关闭aside

--- CoCo-UI/src/App.vue
+++ CoCo-UI/src/App.vue
@@ -5,12 +5,18 @@
 
 <script lang="ts">
 import { provide, ref } from "vue";
+import { router } from "./router";
 export default {
   name: "App",
   setup() {
     const width = document.documentElement.clientWidth;
     const menuVisible = ref(width <= 500 ? false : true); //menuVisible值默认为false 页面宽度小于500则为手机端
     provide("menuVisible", menuVisible); //set  menuVisible取名为menuVisible provide设置的变量，任意子组件均可访问
+    router.afterEach(() => {
+      if (width <= 500) {
+        menuVisible.value = false; //router.afterEach路由切换方法 路由切换后menuVisible.value为false 侧边栏在手机端关闭
+      }
+    });
   },
 };
 </script>
---
+++ CoCo-UI/src/router.ts
@@ -0,0 +1,24 @@
+/**
+ * 手机端路由切换之后关闭aside
+ * 将router封装成单独的router.ts
+ * 在App.vue中引入router.ts
+ * router.afterEach监听到路由改变方法中改变menuVisible.value的值，即可在路由切换时将侧边栏收起(手机端)
+ */
+import { createWebHashHistory, createRouter } from 'vue-router'
+import Home from './views/Home.vue'
+import Doc from './views/Doc.vue'
+import SwitchDemo from './components/SwitchDemo.vue'
+
+const history = createWebHashHistory()
+export const router = createRouter({
+  history: history,
+  routes: [
+    { path: '/', component: Home },
+    {
+      path: '/doc', component: Doc, children: [
+        { path: 'switch', component: SwitchDemo }
+      ]
+
+    }
+  ]
+});
