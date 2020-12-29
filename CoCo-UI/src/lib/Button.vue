ad26dda23efaf1883058dd5320d10845ed9a1938
jkfan2 <jkfan2@iflytek.com>
2020-12-29 19:46:18 +0800

    支持level属性

--- CoCo-UI/src/lib/Button.vue
+++ CoCo-UI/src/lib/Button.vue
@@ -1,16 +1,36 @@
 
 <template>
-  <button class="coco-button" :class="`coco-theme-${theme}`">
+  <button class="coco-button" :class="classes">
     <slot />
   </button>
 </template>
 <script lang="ts">
+import { computed } from "vue";
 export default {
   props: {
     theme: {
       type: String,
       default: "button",
     },
+    size: {
+      type: String,
+      default: "normal",
+    },
+    level: {
+      type: String,
+      default: "normal",
+    },
+  },
+  setup(props) {
+    const classes = computed(() => {
+      const { theme, size, level } = props;
+      return {
+        [`coco-theme-${theme}`]: theme,
+        [`coco-size-${size}`]: size,
+        [`coco-level-${level}`]: level,
+      };
+    });
+    return { classes };
   },
 };
 </script>
@@ -19,6 +39,7 @@ $h: 32px;
 $border-color: #d9d9d9;
 $color: #333;
 $blue: #40a9ff;
+$red: red;
 $radius: 4px;
 .coco-button {
   box-sizing: border-box;
@@ -34,6 +55,7 @@ $radius: 4px;
   border: 1px solid $border-color;
   border-radius: $radius;
   box-shadow: 0 1px 0 fade-out(black, 0.95);
+  transition: background 250ms;
   & + & {
     margin-left: 8px;
   }
@@ -47,6 +69,107 @@ $radius: 4px;
   }
   &::-moz-focus-inner {
     border: 0;
+  }
+  &.coco-theme-link {
+    border-color: transparent;
+    box-shadow: none;
+    color: $blue;
+    &:hover,
+    &:focus {
+      color: lighten($blue, 10%);
+    }
+  }
+  &.coco-theme-text {
+    border-color: transparent;
+    box-shadow: none;
+    color: inherit;
+    &:hover,
+    &:focus {
+      background: darken(white, 5%);
+    }
+  }
+  &.coco-theme-button {
+    &.coco-size-big {
+      font-size: 24px;
+      height: 48px;
+      padding: 0 16px;
+    }
+    &.coco-size-small {
+      font-size: 12px;
+      height: 20px;
+      padding: 0 4px;
+    }
+  }
+  &.coco-theme-link {
+    &.coco-size-big {
+      font-size: 24px;
+      height: 48px;
+      padding: 0 16px;
+    }
+    &.coco-size-small {
+      font-size: 12px;
+      height: 20px;
+      padding: 0 4px;
+    }
+  }
+  &.coco-theme-text {
+    &.coco-size-big {
+      font-size: 24px;
+      height: 48px;
+      padding: 0 16px;
+    }
+    &.coco-size-small {
+      font-size: 12px;
+      height: 20px;
+      padding: 0 4px;
+    }
+  }
+  &.coco-theme-button {
+    &.coco-level-main {
+      background: $blue;
+      color: white;
+      border-color: $blue;
+      &:hover,
+      &:focus {
+        background: darken($blue, 10%);
+        border-color: darken($blue, 10%);
+      }
+    }
+    &.coco-level-danger {
+      background: $red;
+      border-color: $red;
+      color: white;
+      &:hover,
+      &:focus {
+        background: darken($red, 10%);
+        border-color: darken($red, 10%);
+      }
+    }
+  }
+  &.coco-theme-link {
+    &.coco-level-danger {
+      color: $red;
+      &:hover,
+      &:focus {
+        color: darken($red, 10%);
+      }
+    }
+  }
+  &.coco-theme-text {
+    &.coco-level-main {
+      color: $blue;
+      &:hover,
+      &:focus {
+        color: darken($blue, 10%);
+      }
+    }
+    &.coco-level-danger {
+      color: $red;
+      &:hover,
+      &:focus {
+        color: darken($red, 10%);
+      }
+    }
   }
 }
 </style>
