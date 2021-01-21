c89c9b9a8b242be2f1c8895362549e328704df74
jkfan2 <jkfan2@iflytek.com>
2021-01-21 18:18:31 +0800

    实现下滑线自适应与滑动

--- CoCo-UI/src/lib/Tabs.vue
+++ CoCo-UI/src/lib/Tabs.vue
@@ -1,9 +1,10 @@
 <template>
   <div class="coco-tabs">
-    <div class="coco-tabs-nav">
-      <div class="coco-tabs-nav-item" :class="{selected:t===selected}" v-for="(t,index) in titles" @click="select(t)"
-        :key="index">{{t}}
+    <div class="coco-tabs-nav" ref="container">
+      <div class="coco-tabs-nav-item" :class="{selected:t===selected}" v-for="(t,index) in titles"
+        :ref="el=>{if(el) navItems[index]=el}" @click="select(t)" :key="index">{{t}}
       </div>
+      <div class="coco-tabs-nav-indicator" ref="indicator"></div>
     </div>
     <div class="coco-tabs-content">
       <!-- component实现插槽效果 -->
@@ -13,7 +14,7 @@
   </div>
 </template>
 <script lang="ts">
-import { computed } from "vue";
+import { computed, onMounted, onUpdated, ref } from "vue";
 import Tab from "./Tab.vue";
 export default {
   props: {
@@ -22,6 +23,25 @@ export default {
     },
   },
   setup(props, context) {
+    const navItems = ref<HTMLDivElement[]>([]);
+    const indicator = ref<HTMLDivElement>(null);
+    const container = ref<HTMLDivElement>(null);
+    const x = () => {
+      const divs = navItems.value;
+      const result = divs.filter((div) =>
+        div.classList.contains("selected")
+      )[0];
+      const { width } = result.getBoundingClientRect(); //获取选中元素的宽度
+      indicator.value.style.width = width + "px"; //将元素宽度赋给下划线
+      const { left: left1 } = container.value.getBoundingClientRect();
+      const { left: left2 } = result.getBoundingClientRect();
+      const left = left2 - left1;
+      indicator.value.style.left = left + "px"; //下划线的位置
+    };
+    //需要在挂载之后执行 只在第一次渲染执行
+    onMounted(x);
+    //更新时渲染
+    onUpdated(x);
     //在运行是确认子组件的类型 检查context.slots.default()数组的每一项type
     const defaults = context.slots.default();
     defaults.forEach((tags) => {
@@ -41,7 +61,7 @@ export default {
     const select = (title: String) => {
       context.emit("update:selected", title);
     };
-    return { defaults, titles, select };
+    return { navItems, indicator, container, defaults, titles, select };
   },
 };
 </script>
@@ -54,6 +74,7 @@ $border-color: #d9d9d9;
     display: flex;
     color: $color;
     border-bottom: 1px solid $border-color;
+    position: relative;
     &-item {
       padding: 8px 0;
       margin: 0 16px;
@@ -64,6 +85,15 @@ $border-color: #d9d9d9;
       &.selected {
         color: $blue;
       }
+    }
+    &-indicator {
+      position: absolute;
+      height: 3px;
+      background: $blue;
+      left: 0;
+      bottom: -1px;
+      width: 100px;
+      transition: all 250ms; //250ms是动画最舒服的时间
     }
   }
   &-content {
