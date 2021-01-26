e617e6ad67f6d1c5b51a3f647835fe7853ed07da
jkfan2 <jkfan2@iflytek.com>
2021-01-26 20:12:02 +0800

    事不过三

--- CoCo-UI/src/main.ts
+++ CoCo-UI/src/main.ts
@@ -18,7 +18,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import { router } from './router';
 import './lib/svg.js'
+import 'github-markdown-css'
+import Markdown from './components/Markdown.vue';
 
 const app = createApp(App)
 app.use(router)
 app.mount('#app')
+app.component("Markdown", Markdown)  //全局引入Markdown，不需要单独引入
